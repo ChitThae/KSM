6d8680d6692afb5a98a00a4616a7f9f90e8ec27b
Administrator <Administrator@MR-SONT69GN1IE9>
2017-11-23 21:33:06 -0800

    New project

---
+++ BestFriends/src/com/salt/jdc/Bitches.java
@@ -0,0 +1,34 @@
+package com.salt.jdc;
+
+public class Bitches {
+	private String name;
+	private int age;
+	private int rollno;
+	private String address;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public int getRollno() {
+		return rollno;
+	}
+	public void setRollno(int rollno) {
+		this.rollno = rollno;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+
+}
